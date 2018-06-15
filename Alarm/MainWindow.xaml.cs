7bc68b53d650ef33f8b120d529f3c623d713ebfb
Draug <draug555@yandex.ru>
2018-06-15 16:21:37 +0600

    Last version

--- Alarm/MainWindow.xaml.cs
+++ Alarm/MainWindow.xaml.cs
@@ -1,17 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Threading;
+using System.Xml;
 
 namespace Alarm
 {
@@ -20,14 +13,106 @@ namespace Alarm
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        static public string file_path;
+        static public double time = 9;
+        private DateTime endTime;
+        private DispatcherTimer timer;
+        private MediaPlayer mp;
+
         public MainWindow()
         {
             InitializeComponent();
+            labelClock.MouseDown += (s, e) => DragMove();
+            InitializeSettings();
         }
 
-        private void Button_Exit_Click(object sender, RoutedEventArgs e)
+        private void buttonExit_Click(object sender, RoutedEventArgs e)
         {
-            
+            this.Close();
+        }
+
+        private void buttonSettings_Click(object sender, RoutedEventArgs e)
+        {
+            Settings settings = new Settings
+            {
+                ShowInTaskbar = false
+            };
+            settings.ShowDialog();
+        }
+
+        private void buttonStart_Click(object sender, RoutedEventArgs e)
+        {
+            timer = new DispatcherTimer
+            {
+                Interval = new TimeSpan(0, 0, 1)
+            };
+            timer.Tick += new EventHandler(CheckTime);
+            mp = new MediaPlayer();
+            DateTime date = DateTime.Now;
+            endTime = date.AddHours(time);
+            timer.Start();
+            buttonStart.IsEnabled = false;
+        }
+
+        private void CheckTime(object sender, EventArgs e)
+        {
+            DateTime date = DateTime.Now;
+            TimeSpan time_dif = new TimeSpan(endTime.Ticks - date.Ticks);
+            if (time_dif.Ticks <= 0)
+            {
+                DoAlarm();
+            }
+            labelClock.Content = time_dif.ToString(@"hh\:mm\:ss");
+        }
+
+        private void DoAlarm()
+        {
+            timer.Stop();
+            if (File.Exists(file_path))
+            {
+                mp.Open(new Uri(file_path));
+                mp.Play();
+            }
+            buttonStart.IsEnabled = true;
+        }
+
+        public void StopTimer()
+        {
+            mp.Stop();
+            timer.Stop();
+        }
+
+        private void InitializeSettings()
+        {
+            XmlDocument xmlDocument = new XmlDocument();
+            bool find_file = false;
+            while (!find_file)
+            {
+                try
+                {
+                    xmlDocument.Load(Settings.pathToXml);
+                    XmlNodeList elementList = xmlDocument.GetElementsByTagName("head");
+                    if (elementList[0].InnerXml != "")
+                    {
+                        XmlNode node = xmlDocument.SelectSingleNode("//*[@id = 'time']");
+                        MainWindow.time = Convert.ToDouble(node.InnerText);
+                        node = xmlDocument.SelectSingleNode("//*[@id = 'music']");
+                        MainWindow.file_path = Convert.ToString(node.InnerText);
+                    }
+                    find_file = true;
+                }
+                catch (System.IO.FileNotFoundException e)
+                {
+                    MessageBox.Show("Перед использованием, настройте будильник");
+                    XmlTextWriter textWritter = new XmlTextWriter(Settings.pathToXml, Encoding.UTF8);
+                    textWritter.WriteStartDocument();
+                    textWritter.WriteStartElement("head");
+                    textWritter.WriteEndElement();
+                    textWritter.Close();
+                    find_file = true;
+                }
+            }
         }
     }
 }
---
+++ Alarm/Settings.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Text;
+using System.Windows;
+using System.Xml;
+
+namespace Alarm
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Settings : Window
+    {
+
+        XmlDocument xmlDocument = new XmlDocument();
+        static public string pathToXml = "settings.xml";
+
+        public Settings()
+        {
+            InitializeComponent();
+            InitializeSettings();
+        }
+
+        private void InitializeSettings()
+        {
+            bool find_file = false;
+            while (!find_file)
+            {
+                try
+                {
+                    xmlDocument.Load(pathToXml);
+                    XmlNodeList elementList = xmlDocument.GetElementsByTagName("head");
+                    if(elementList[0].InnerXml == "")
+                    {
+                        XmlNode element = xmlDocument.CreateElement("data");
+                        XmlAttribute attribute = xmlDocument.CreateAttribute("id");
+                        attribute.Value = "time";
+                        element.InnerText = Convert.ToString(MainWindow.time);
+                        element.Attributes.Append(attribute);
+                        xmlDocument.DocumentElement.AppendChild(element);
+                        element = xmlDocument.CreateElement("data");
+                        attribute = xmlDocument.CreateAttribute("id");
+                        attribute.Value = "music";
+                        element.Attributes.Append(attribute);
+                        element.InnerText = Convert.ToString(MainWindow.file_path);
+                        xmlDocument.DocumentElement.AppendChild(element);
+                        xmlDocument.Save(pathToXml);
+                    } else
+                    {
+                        XmlNode node = xmlDocument.SelectSingleNode("//*[@id = 'time']");
+                        MainWindow.time = Convert.ToDouble(node.InnerText);
+                        node = xmlDocument.SelectSingleNode("//*[@id = 'music']");
+                        MainWindow.file_path = Convert.ToString(node.InnerText);
+                        tbMaxTime.Text = Convert.ToString(MainWindow.time);
+                        strFilePath.Text = Convert.ToString(MainWindow.file_path);
+                    }
+                    find_file = true;
+                }
+                catch (System.IO.FileNotFoundException e)
+                {
+                    XmlTextWriter textWritter = new XmlTextWriter(pathToXml, Encoding.UTF8);
+                    textWritter.WriteStartDocument();
+                    textWritter.WriteStartElement("head");
+                    textWritter.WriteEndElement();
+                    textWritter.Close();
+                    find_file = true;
+                }
+            }
+        }
+
+        private void buttonGetFile_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.DefaultExt = ".mp3";
+            dlg.Filter = "MP3 files (.mp3)|*.mp3|Wav files (.wav)|*.wav";
+            Nullable<bool> result = dlg.ShowDialog();
+            if(result == true)
+            {
+                string filename = dlg.FileName;
+                MainWindow.file_path = filename;
+                strFilePath.Text = filename;
+            }
+        }
+
+        private void buttonSaveSettings_Click(object sender, RoutedEventArgs e)
+        {
+            double time = Double.Parse(tbMaxTime.Text);
+            MainWindow.time = time;
+
+            XmlNode node = xmlDocument.SelectSingleNode("//*[@id = 'time']");
+            node.InnerText = Convert.ToString(MainWindow.time);
+            node = xmlDocument.SelectSingleNode("//*[@id = 'music']");
+            node.InnerText = Convert.ToString(MainWindow.file_path);
+            xmlDocument.Save(pathToXml);
+
+            Close();
+        }
+    }
+}
